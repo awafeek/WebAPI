06467b3e4b1f24597c4fa31c2bc1fe0336b025fd
awafeek <awafeek@getgroup.com>
2021-10-13 13:12:29 +0200

    testing local regitery update

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -31,7 +31,7 @@ namespace WebAPI.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)]+"1"
             })
             .ToArray();
         }
